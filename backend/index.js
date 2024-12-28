fd1af34655d347f86a4a269151f8c1c5f64efdb2
Aayush <aayush@Aayushs-MacBook-Pro.local>
2024-12-28 18:04:33 +0530

    checking pipeline trigger

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app = express();
  const cors = require('cors');
 app.use(cors({ origin: '*' })); // Allow frontend origin
 
+
 const quotes = [
     "The only way to do great work is to love what you do. - Steve Jobs",
     "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
